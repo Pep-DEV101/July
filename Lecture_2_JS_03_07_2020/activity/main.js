1a19b0641c7e8cdca3d4e344e454d258ecb23d56
Jasbir <mr.jasbirsingh96@gmail.com>
2020-07-04 13:15:54 +0530

    final webscrapping code

--- Lecture_2_JS_03_07_2020/activity/main.js
+++ Lecture_2_JS_03_07_2020/activity/main.js
@@ -1,12 +1,10 @@
 let request = require("request");
 let fs = require("fs");
-let MatchRef = require("./match.js");
+let AllMatchRef = require("./allMatch.js");
 let cheerio = require("cheerio");
+let url = "https://www.espncricinfo.com/series/_/id/8039/season/2015/icc-cricket-world-cup";
 console.log("Request send");
-function processAllMatch(url) {
-
-    request(url, cb);
-}
+request(url, cb);
 function cb(err, header, body) {
     if (err == null && header.statusCode == 200) {
         console.log("Recieved Data");
@@ -19,19 +17,15 @@ function cb(err, header, body) {
         console.log(header);
     }
 }
+
 function parsehtml(data) {
     let ch = cheerio.load(data);
-    let cards = ch(".col-md-8.col-16");
-    for (let i = 0; i < cards.length; i++) {
-        let anchorsArr = ch(cards[i]).find(".match-cta-container a");
-        let link = ch(anchorsArr[0]).attr("href");
-        // console.log(matchLink);
-        let completeLink = "https://www.espncricinfo.com" + link;
-        // console.log(completeLink);
-        MatchRef.processMatch(completeLink);
-    }
+    let anchors = ch(".widget-items.cta-link .label.blue-text.blue-on-hover");
+    let link = anchors.attr("href");
+    // console.log(matchLink);
+    let completeLink = "https://www.espncricinfo.com" + link;
+    // console.log(completeLink);
+    AllMatchRef.processAllMatch(completeLink);
+
     // console.log("Saved All cards");
-}
-module.exports = {
-    processAllMatch: processAllMatch
 }
--- Lecture_2_JS_03_07_2020/activity/match.js
+++ Lecture_2_JS_03_07_2020/activity/match.js
@@ -1,6 +1,7 @@
 let request = require("request");
-let fs = require("fs");
+let fs = require("fs"); //file system
 let cheerio = require("cheerio");
+let path = require("path"); // nodejs module
 function processMatch(url) {
     console.log("Request send");
     request(url, cb);
@@ -20,9 +21,9 @@ function cb(err, header, body) {
 function parsehtml(body) {
     let ch = cheerio.load(body);
     let matchResultElement = ch(".desc.text-truncate");
-    console.log(matchResultElement.text());
+    let result = matchResultElement.text().trim();
     let matchSummaryElem = ch(".summary span");
-    console.log(matchSummaryElem.text());
+    let summary = matchSummaryElem.text().trim();
     let cardsElems = ch(".card.content-block.match-scorecard-table .Collapsible");
     // console.log(cardsElems.length);
     // fs.writeFileSync("innings.html", cardsElems);
@@ -30,27 +31,84 @@ function parsehtml(body) {
     console.log("###################################");
     for (let i = 0; i < cardsElems.length; i++) {
         let teamNameElem = ch(cardsElems[i]).find("h5");
-        let cTeam = teamNameElem.text()
-        cTeam = cTeam.split("Innings")[0]
-        let AllRows = ch(cardsElems[i]).find(".table.batsman tbody tr")
+        let cTeam = teamNameElem.text();
+        cTeam = cTeam.split("Innings")[0];
+        cTeam = cTeam.trim();
+        let AllRows = ch(cardsElems[i]).find(".table.batsman tbody tr");
         for (let j = 0; j < AllRows.length; j++) {
             let AllCols = ch(AllRows[j]).find("td");
             if (AllCols.length > 1) {
                 // player waali row
                 let cols = ch(AllRows[j]).find("td");
-                let batsmanName = ch(cols[0]).text();
-                let runs = ch(cols[2]).text();
-                let balls = ch(cols[3]).text();
-                let fours = ch(cols[5]).text();
-                let sixes = ch(cols[6]).text();
-                let sr = ch(cols[7]).text();
+                let batsmanName = ch(cols[0]).text().trim();
+                let runs = ch(cols[2]).text().trim();
+                let balls = ch(cols[3]).text().trim();
+                let fours = ch(cols[5]).text().trim();
+                let sixes = ch(cols[6]).text().trim();
+                let sr = ch(cols[7]).text().trim();
                 console.log(`Teams: ${cTeam} Name ${batsmanName} Runs: ${runs}  Balls: ${balls} Fours: ${fours} sixes: ${sixes} sr: ${sr}`);
+                processPlayer(cTeam, batsmanName, runs, balls, fours, sixes, sr, summary, result);
             }
         }
         console.log("````````````````````````````````````````````````");
 
     }
 }
+// processMatch("https://www.espncricinfo.com/series/8039/scorecard/656495/australia-vs-new-zealand-final-icc-cricket-world-cup-2014-15");
 module.exports = {
     processMatch: processMatch
+}
+function checkIfDirExist(dest) {
+    return fs.existsSync(dest);
+}
+function checkIfFileExist(dest) {
+    return fs.existsSync(dest);
+}
+function createDir(dest) {
+    return fs.mkdirSync(dest);
+}
+// \\team\\batsManName.json
+function processPlayer(team, batsmanName, runs, balls, fours, sixes, sr, summary, result) {
+    // 1.check =>team folder
+    let filePath = path.join(team, batsmanName + ".json");
+    let details = {
+        Runs: runs,
+        Balls: balls,
+        Sixes: sixes,
+        SR: sr,
+        Summary: summary,
+        Result: result,
+        Fours: fours,
+        BatsManName: batsmanName
+    }
+    let dirPath = team
+    let dirExist = checkIfDirExist(dirPath);
+    if (dirExist) {
+        // yes=> check player ki file
+        // yes=> update
+        // create => data enter
+        let entries;
+        let fileExist = checkIfFileExist(filePath);
+        if (fileExist) {
+            // update file
+            let content = fs.readFileSync(filePath);
+            entries = JSON.parse(content);
+            entries.push(details);
+        } else {
+            // create file
+            entries = [];
+            entries.push(details);
+        }
+        let data = JSON.stringify(entries);
+        fs.writeFileSync(filePath, data);
+
+    } else {
+        // No => create folder
+        createDir(dirPath);
+        // create file and add data
+        let entries = [];
+        entries.push(details);
+        let data = JSON.stringify(entries);
+        fs.writeFileSync(filePath, data);
+    }
 }
