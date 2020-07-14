896241c4e3bae726e144dedc5c13bc0eac71fdb3
Jasbir <mr.jasbirsingh96@gmail.com>
2020-07-14 13:31:39 +0530

    updated code upto create Challenge

--- Lecture_7_JS_14_07_2020/activity/puppeteer.js
+++ Lecture_7_JS_14_07_2020/activity/puppeteer.js
@@ -1,6 +1,6 @@
 let puppeteer = require("puppeteer");
 // npm install puppeteer
-let gPage, glangCodeElems;
+let gPage;
 let { email, password } = require("../../credentials.json");
 (async function () {
     try {
@@ -11,16 +11,29 @@ let { email, password } = require("../../credentials.json");
         })
         let pages = await browser.pages();
         let page = pages[0];
-        gPage=page
+        gPage = page
         await page.goto("https://www.hackerrank.com/auth/login");
         await page.type("#input-1", email);
         await page.type("#input-2", password);
         await navigatorFn("button[data-analytics='LoginPassword']");
+        await page.waitForSelector("a[data-analytics='NavBarProfileDropDown']", { visible: true });
+        await page.click("a[data-analytics='NavBarProfileDropDown']");
+        await page.waitForSelector("a[data-analytics='NavBarProfileDropDownAdministration']", { visible: true });
+        await navigatorFn("a[data-analytics='NavBarProfileDropDownAdministration']")
+        await page.waitForSelector(".nav-tabs.nav.admin-tabbed-nav li", { visible: true });
+        let allTabs = await page.$$(".nav-tabs.nav.admin-tabbed-nav li");
+        await allTabs[1].click();
+        let challenges = require("./challenge");
+        let createChallengePageLink = page.url();
+        for (let i = 0; i < challenges.length; i++) {
+            await page.goto(createChallengePageLink);
+
+            await createChallenge(challenges[i]);
+        }
         console.log("Navigated");
     } catch (err) {
         console.log(err);
     }
-
 })();
 
 async function navigatorFn(selector) {
@@ -33,4 +46,9 @@ async function navigatorFn(selector) {
         return err;
     }
 
+}
+
+async function createChallenge(ch) {
+    await gPage.waitForSelector(".btn.btn-green.backbone.pull-right", { visible: true });
+    await navigatorFn(".btn.btn-green.backbone.pull-right");
 }
