2eb9b6cd3053ad9dca6e2a4ca4f95d9f47011431
Jasbir <mr.jasbirsingh96@gmail.com>
2020-07-22 19:20:10 +0530

    implemented new and save

--- Lecture_8_UI_Excel/activity/index.js
+++ Lecture_8_UI_Excel/activity/index.js
@@ -1,7 +1,9 @@
 const $ = require("jquery");
+const dialog = require("electron").remote.dialog;
+const fs = require("fs");
 $(document).ready(function () {
     // console.log("Jquery loaded on ui");
-
+    let db;
     $("#grid .cell").on("click", function () {
         let rowId = Number($(this).attr("rowId")) + 1;
         let colId = Number($(this).attr("colId")) + 65;
@@ -24,9 +26,54 @@ $(document).ready(function () {
     })
 
     $(".menu").on("click", function () {
-     let menuName=$(this).attr("id");
+        let menuName = $(this).attr("id");
 
-    $(".menu-options").removeClass("active");
-     $("#"+menuName+"-menu-options").addClass("active");
+        $(".menu-options").removeClass("active");
+        $("#" + menuName + "-menu-options").addClass("active");
     })
+
+
+    // *****************************New-Open-Save***************
+    $("#New").on("click", function () {
+        db = []
+        let allRows = $("#grid .row");
+        for (let i = 0; i < allRows.length; i++) {
+            let row = [];
+            let allCellsofaR = $(allRows[i]).find(".cell");
+            for (let j = 0; j < allCellsofaR.length; j++) {
+                $(allCellsofaR[j]).html("");
+                let cell = "";
+                row.push(cell);
+            }
+            db.push(row);
+        }
+        // console.log(ts);
+    })
+    $("#Save").on("click", function () {
+        // console.log(db);
+        let fPath = dialog.showSaveDialogSync();
+        let sData = JSON.stringify(db);
+        fs.writeFileSync(fPath, sData);
+        alert("File saved");
+        // open dialog box 
+        // save db into a file
+    })
+
+    $("#grid .cell").on("blur", function () {
+        let { rowId, colId } = getRcfromElem(this);
+        db[rowId][colId] = $(this).html();
+    })
+    function getRcfromElem(elem) {
+        let rowId = $(elem).attr("rowId");
+        let colId = $(elem).attr("colId");
+        return {
+            rowId,
+            colId
+        }
+    }
+    function init() {
+$("#File").trigger("click");
+$("#New").trigger("click");
+    }
+    init();
 })
